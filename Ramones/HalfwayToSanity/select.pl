e1fa7de352f07519ca025402a42fce2e31508497
Russ Johnson <nofuss@gmail.com>
2016-03-25 12:42:09 +0100

    worked out I know better now

--- Ramones/HalfwayToSanity/select.pl
+++ Ramones/HalfwayToSanity/select.pl
@@ -3,11 +3,7 @@ use strict;
 use warnings;
 
 my @songs = qw(
-01-IWannaLive
-02-BopTilYouDrop
 03-GardenOfSerenity
-04-WeaselFace
-05-GoLilCamaro
 06-IKnowBetterNow
 07-DeathOfMe
 08-ILostMyMind
@@ -15,6 +11,13 @@ my @songs = qw(
 10-ImNotJesus
 11-ByeByeBaby
 12-WormMan
+);
+
+my @doneSongs = qw(
+01-IWannaLive
+02-BopTilYouDrop
+04-WeaselFace
+05-GoLilCamaro
 );
 
 print @songs[rand @songs] . "\n";
