ed23dd6ee57971d8fa70d4d2dc72e5707956c2bf
Russ Johnson <nofuss@gmail.com>
2016-04-03 13:15:06 +0200

    cheap and disgusting at its best

---
+++ TheLillingtons/DeathByTelevision/select.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-WarOfTheWorlds
+02-DontTrustTheHumanoids
+03-BlackHoleInMyMind
+04-ISawTheApeman
+05-X-RaySpecs
+06-InvasionOfTheSaucerman
+07-YoureTheOnlyOne
+08-INeedSomeBrainDamage
+09-CodenamePeabrain
+10-PhantomMaggot
+11-RobotsInMyDreams
+12-MurderOnMyMind
+13-Caveman
+14-ICameFromTheFuture
+);
+
+my @songsDone = qw(
+);
+
+
+print $songsToDo[rand @songsToDo] . "\n";
