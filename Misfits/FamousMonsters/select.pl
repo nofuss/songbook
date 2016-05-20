dfac60f7679df93445ec150890d770deff8ee05c
Russ Johnson <nofuss@gmail.com>
2016-05-20 12:17:40 +0200

    Setting things up to work on Famous Monsters

---
+++ Misfits/FamousMonsters/select.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-KongAtTheGates
+02-TheForbiddenZone
+03-LostInSpace
+04-DustToDust
+05-CrawlingEye
+06-WitchHunt
+07-Scream
+08-SaturdayNight
+09-PumpkinHead
+10-ScarecrowMan
+11-DieMonsterDie
+12-LivingHell
+13-DescendingAngel
+14-Them
+15-FiendClub
+16-HuntingHumans
+17-Helena
+18-KongUnleashed
+);
+
+my @songsDone = qw(
+);
+
+
+print $songsToDo[rand @songsToDo] . "\n";
