7fa5f0a3f46d67043a64b4e889f7c25c09ebd511
Russ Johnson <nofuss@gmail.com>
2016-06-18 12:48:27 +0200

    reviewed Glad to See you Go and realized I got it right the first time

--- Ramones/LeaveHome/select.pl
+++ Ramones/LeaveHome/select.pl
@@ -3,24 +3,23 @@ use strict;
 use warnings;
 
 my @songsToDo = qw(
-    01-GladToSeeYouGo
     02-GimmeGimmeShockTreatment
     03-IRememberYou
     04-OhOhILoveHerSo
     05-CarbonaNotGlue
     06-SuzyIsAHeadBanger
     07-Pinhead
-    08-NowIWannaBeAGoodBoy
     09-SwallowMyPride
     10-WhatsYourGame
     11-CaliforniaSun
     12-Commando
     13-YoureGonnaKillThatGirl
     14-YouShouldNeverHaveOpenedThatDoor
-
 );
 
 my @songsDone = qw(
+    01-GladToSeeYouGo
+    08-NowIWannaBeAGoodBoy
 );
 
 
