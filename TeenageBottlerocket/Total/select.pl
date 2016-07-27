b59cbedba2f62212907b131c88efa5f162a8849f
Russ Johnson <nofuss@gmail.com>
2016-07-27 19:52:38 +0200

    setting up TeenageBottlerocket - Total album

---
+++ TeenageBottlerocket/Total/select.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-Radio
+02-SoCool
+03-StupidGames
+04-FallForMe
+05-Crashing
+06-LostInSpace
+07-GoAway
+08-Rebound
+09-BloodBathAtBurgerKing
+10-Veronica
+11-RepeatOffender
+12-ABomb
+13-SoFarAway
+);
+
+my @songsDone = qw(
+);
+
+for my $song (@songsToDo) {
+    print `mkdir $song`;
+    print `cp tabs.et6 $song`;
+}
+
+#print $songsToDo[rand @songsToDo] . "\n";
