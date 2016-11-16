f99c740c0fafd1c0b112f9846d1ad3eddc1774e1
Russ Johnson <nofuss@gmail.com>
2016-11-16 22:48:27 +0100

    finished off Fiend Club

--- Misfits/FamousMonsters/select.pl
+++ Misfits/FamousMonsters/select.pl
@@ -6,14 +6,13 @@ my @songsToDo = qw(
 01-KongAtTheGates
 04-DustToDust
 06-WitchHunt
-15-FiendClub
+07-Scream
 );
 
 my @songsDone = qw(
 02-TheForbiddenZone
 03-LostInSpace
 05-CrawlingEye
-07-Scream
 08-SaturdayNight
 09-PumpkinHead
 10-ScarecrowMan
@@ -21,6 +20,7 @@ my @songsDone = qw(
 12-LivingHell
 13-DescendingAngel
 14-Them
+15-FiendClub
 16-HuntingHumans
 17-Helena
 18-KongUnleashed
