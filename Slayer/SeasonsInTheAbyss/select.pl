937c1c1830358184131a93fc6a9ba5e943cd6ca4
Russ Johnson <nofuss@gmail.com>
2017-03-17 21:04:14 +0100

    worked out tabs for intro to seasons in the abyss

--- Slayer/SeasonsInTheAbyss/select.pl
+++ Slayer/SeasonsInTheAbyss/select.pl
@@ -18,9 +18,9 @@ my @songsToDo = qw(
 my @songsDone = qw(
 );
 
-for my $song (@songsToDo) {
-    print `mkdir $song`;
-    print `cp tabs.et6 $song`;
-}
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+#}
 
-#print $songsToDo[rand @songsToDo] . "\n";
+print $songsToDo[rand @songsToDo] . "\n";
