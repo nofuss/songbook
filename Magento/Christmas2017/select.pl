146a3fcfd2b31d2c9e57ef3b9b80da040ebc399f
Russ Johnson <nofuss@gmail.com>
2017-05-19 09:22:51 +0200

    setting up work on christmas show

---
+++ Magento/Christmas2017/select.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+DireStraits-SultansOfSwing
+JohnFogerty-RockinAllOverTheWorld
+ThePolice-MessageInABottle
+JarabeDePalo-LaFlaca
+JoanJettAndTheBlackhearts-ILoveRockNRoll
+Aerosmith-WalkThisWay
+BonJovi-LivinOnAPrayer
+AcDc-Thunderstruck
+MikeOldfield-MoonlightShadow
+);
+
+my @songsDone = qw(
+
+);
+
+for my $song (@songsToDo) {
+    print `mkdir $song`;
+    print `cp tabs.et6 $song`;
+}
+
+print $songsToDo[rand @songsToDo] . "\n";
