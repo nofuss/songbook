7d70d267e476eaaba2bed1822fcd6ead041ede77
Russ Johnson <nofuss@gmail.com>
2017-06-17 14:03:17 +0200

    worked out welcome to the nuthouse

--- TeenageBottlerocket/WarningDevice/select.pl
+++ TeenageBottlerocket/WarningDevice/select.pl
@@ -9,18 +9,17 @@ my @songsToDo = qw(
 04-ShesNotTheOne
 05-Pacemaker
 06-SocialLife
-07-WelcomeToTheNuthouse
 08-AnnasSong
 09-OnMyOwn
 10-TotallyStupid
-11-CrawlingBackToYou
 12-WarningDevice
 13-WastingTime
 14-IKnowYouKnow
 );
 
 my @songsDone = qw(
-
+07-WelcomeToTheNuthouse
+11-CrawlingBackToYou
 );
 
 #for my $song (@songsToDo) {
