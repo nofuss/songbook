d3d733d92694a1a71f390a4af5fdd19eb668050c
Russ Johnson <nofuss@gmail.com>
2017-06-19 23:07:35 +0200

    getting started on project 313

---
+++ TheLillingtons/Project313/select.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-UntilTheSunShines
+02-RubberRoom
+03-Project313
+);
+
+my @songsDone = qw(
+);
+
+for my $song (@songsToDo) {
+    print `mkdir $song`;
+    print `cp tabs.et6 $song`;
+}
+
+print $songsToDo[rand @songsToDo] . "\n";
