e1400a4d3293651719a3cab2f324b567e06f7e71
Russ Johnson <nofuss@gmail.com>
2017-12-02 13:55:06 +0100

    setting up a christmas favorite

---
+++ Deicide/OnceUponTheCross/select.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-OnceUponTheCross
+02-ChristDenied
+03-WhenSatanRulesHisWorld
+04-KillTheChristian
+05-TrickOrBetrayed
+06-TheyAreTheChildrenOfTheUnderworld
+07-BehindTheLightThouShallRise
+08-ToBeDead
+09-ConfessionalRape
+);
+
+my @songsDone = qw(
+
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
