94fe340afe585a18bad9d834bbccdc86be0266bf
Russ Johnson <nofuss@gmail.com>
2018-04-19 19:55:25 +0200

    getting started on behind the light

--- Deicide/OnceUponTheCross/select.pl
+++ Deicide/OnceUponTheCross/select.pl
@@ -5,7 +5,6 @@ use warnings;
 my @songsToDo = qw(
 05-TrickOrBetrayed
 06-TheyAreTheChildrenOfTheUnderworld
-07-BehindTheLightThouShallRise
 08-ToBeDead
 09-ConfessionalRape
 );
@@ -15,6 +14,7 @@ my @songsDone = qw(
 02-ChristDenied
 03-WhenSatanRulesHisWorld
 04-KillTheChristian
+07-BehindTheLightThouShallRise
 );
 
 #for my $song (@songsToDo) {
