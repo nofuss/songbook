b58e1b340def22517da0cfa0fcee17ce3070e41e
Russ Johnson <nofuss@gmail.com>
2018-06-17 12:16:24 +0200

    set up blues for the red sun

---
+++ Kyuss/BluesForTheRedSun/select.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-Thumb
+02-GreenMachine
+03-MoltenUniverse
+04-50MillionYearTripDownsideUp
+05-ThongSong
+06-ApothecariesWeight
+08-FreedomRun
+09-800
+10-Writhe
+11-Capsized
+12-AllensWrench
+13-MondoGenerator
+);
+
+for my $song (@songsToDo) {
+    print `mkdir $song`;
+    print `cp tabs.et6 $song`;
+}
+
+my @songsDone = qw(
+07-CaterpillarMarch
+);
+
+
+print $songsToDo[rand @songsToDo] . "\n";
