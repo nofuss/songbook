c03e8164089cd32e8cf022a555e6fe338ca9187f
Russ Johnson <nofuss@gmail.com>
2019-08-31 19:48:31 +0200

    cleaned up x ray specs

--- TheLillingtons/DeathByTelevision/select.pl
+++ TheLillingtons/DeathByTelevision/select.pl
@@ -9,20 +9,20 @@ my @songsStarted = qw(
 01-WarOfTheWorlds
 02-DontTrustTheHumanoids
 04-ISawTheApeman
-05-X-RaySpecs
 06-InvasionOfTheSaucerman
 07-YoureTheOnlyOne
 08-INeedSomeBrainDamage
 09-CodenamePeabrain
 10-PhantomMaggot
 11-RobotsInMyDreams
-12-MurderOnMyMind
 13-Caveman
 14-ICameFromTheFuture
 );
 
 my @songsDone = qw(
+05-X-RaySpecs
 03-BlackHoleInMyMind
+12-MurderOnMyMind
 );
 
 print $songsStarted[rand @songsStarted] . "\n";
