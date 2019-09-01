beb86c76b0a14ba68330c3b469db8565e9034e9c
Russ Johnson <nofuss@gmail.com>
2019-09-01 11:50:27 +0200

    cleaned up brain damage

--- TheLillingtons/DeathByTelevision/select.pl
+++ TheLillingtons/DeathByTelevision/select.pl
@@ -11,15 +11,15 @@ my @songsStarted = qw(
 04-ISawTheApeman
 06-InvasionOfTheSaucerman
 07-YoureTheOnlyOne
-08-INeedSomeBrainDamage
 09-CodenamePeabrain
 10-PhantomMaggot
 14-ICameFromTheFuture
 );
 
 my @songsDone = qw(
-05-X-RaySpecs
 03-BlackHoleInMyMind
+05-X-RaySpecs
+08-INeedSomeBrainDamage
 11-RobotsInMyDreams
 12-MurderOnMyMind
 13-Caveman
