2ddc390c66c15adcf905655a32537155f2768697
Russ Johnson <nofuss@gmail.com>
2019-09-15 12:03:43 +0200

    cleaned up you're the only one for me

--- TheLillingtons/DeathByTelevision/select.pl
+++ TheLillingtons/DeathByTelevision/select.pl
@@ -9,7 +9,6 @@ my @songsStarted = qw(
 01-WarOfTheWorlds
 04-ISawTheApeman
 06-InvasionOfTheSaucerman
-07-YoureTheOnlyOne
 09-CodenamePeabrain
 10-PhantomMaggot
 );
@@ -18,6 +17,7 @@ my @songsDone = qw(
 02-DontTrustTheHumanoids
 03-BlackHoleInMyMind
 05-X-RaySpecs
+07-YoureTheOnlyOne
 08-INeedSomeBrainDamage
 11-RobotsInMyDreams
 12-MurderOnMyMind
