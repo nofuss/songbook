ebd7ff26ca23a869cafcc0b810fa7bd5b1001548
Russ Johnson <nofuss@gmail.com>
2019-12-20 20:09:41 +0100

    setting up last sabbath albumn

---
+++ BlackSabbath/13/select.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-EndOfTheBeginning
+02-GodIsDead
+03-Loner
+04-Zeitgeist
+05-AgeOfReason
+06-LiveForever
+07-DamagedSoul
+08-DearFather
+);
+
+my @songsDone = qw(
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `touch $song/info.txt`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
