f60056475a908d7b7618f54ffb78ecb79cd430b1
Russ Johnson <nofuss@gmail.com>
2020-02-22 12:12:58 +0100

    setting up south of heaven

---
+++ Slayer/SouthOfHeaven/select.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-SouthOfHeaven
+02-SilentScream
+03-LiveUndead
+04-BehindTheCrookedCross
+05-MandatorySuicide
+06-GhostsOfWar
+07-ReadBetweenTheLies
+08-CleanseTheSoul
+09-DissidentAggressor
+10-SpillTheBlood
+);
+
+my @songsDoing = qw(
+
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+    #print `touch $song/info.txt`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
