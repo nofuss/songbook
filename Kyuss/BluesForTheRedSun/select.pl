a7c9d7b384e0d62d5aa504d02267c13a50c32ca9
Russ Johnson <nofuss@gmail.com>
2020-04-17 18:21:27 +0200

    small cleanup to caterpillar march

--- Kyuss/BluesForTheRedSun/select.pl
+++ Kyuss/BluesForTheRedSun/select.pl
@@ -5,7 +5,6 @@ use warnings;
 my @songsToDo = qw(
 01-Thumb
 02-GreenMachine
-03-MoltenUniverse
 04-50MillionYearTripDownsideUp
 05-ThongSong
 06-ApothecariesWeight
@@ -17,12 +16,13 @@ my @songsToDo = qw(
 13-MondoGenerator
 );
 
-for my $song (@songsToDo) {
-    print `mkdir $song`;
-    print `cp tabs.et6 $song`;
-}
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+#}
 
 my @songsDone = qw(
+03-MoltenUniverse
 07-CaterpillarMarch
 );
 
