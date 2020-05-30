103f79addade2c2e526b8caa08f5fe5242939e01
Russ Johnson <nofuss@gmail.com>
2020-05-30 12:01:58 +0200

    setting up death - symbolic

---
+++ Death/Symbolic/select.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+
+my @songsToDo = qw(
+01-Symbolic
+02-ZeroTolerance
+03-EmptyWords
+04-SacredSerenity
+05-1000Eyes
+06-WithoutJudgement
+07-CrystalMountain
+08-Misanthrope
+09-PerennialQuest
+);
+
+my @songsDone = qw(
+
+);
+
+#for my $dir (@songsToDo) {
+    #print `mkdir $dir`;
+    #print `cp tabs.et6 $dir`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
+
