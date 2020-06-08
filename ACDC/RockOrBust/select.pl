d97aa3abd4cb6bb9616b8fcf269c7b71e067f33f
Russ Johnson <nofuss@gmail.com>
2020-06-08 13:23:33 +0200

    cleaned up rock the house

--- ACDC/RockOrBust/select.pl
+++ ACDC/RockOrBust/select.pl
@@ -8,7 +8,6 @@ my @songsToDo = qw(
 02-PlayBall
 03-RockTheBluesAway
 08-BaptismByFire
-09-RockTheHouse
 11-EmissionControl
 );
 
@@ -16,6 +15,7 @@ my @songsDone = qw(
 04-MissAdventure
 06-GotSomeRockAndRollThunder
 07-HardTimes
+09-RockTheHouse
 10-SweetCandy
 );
 
