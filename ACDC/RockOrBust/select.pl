9c75a4c53735e898e198204250c03ed226590ce3
Russ Johnson <nofuss@gmail.com>
2020-06-08 12:22:15 +0200

    cleaned up miss adventure

--- ACDC/RockOrBust/select.pl
+++ ACDC/RockOrBust/select.pl
@@ -7,13 +7,13 @@ my @songsToDo = qw(
 01-RockOrBust
 02-PlayBall
 03-RockTheBluesAway
-04-MissAdventure
 08-BaptismByFire
 09-RockTheHouse
 11-EmissionControl
 );
 
 my @songsDone = qw(
+04-MissAdventure
 06-GotSomeRockAndRollThunder
 07-HardTimes
 10-SweetCandy
