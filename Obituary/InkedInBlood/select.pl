cc15add24b9a3d5fe7b0f2e0db68703c4a7660b4
Russ Johnson <nofuss@gmail.com>
2020-07-01 12:03:24 +0200

    worked out deny you

--- Obituary/InkedInBlood/select.pl
+++ Obituary/InkedInBlood/select.pl
@@ -8,7 +8,6 @@ my @songsToDo = qw(
 05-BackOnTop
 06-Violence
 07-InkedInBlood
-08-DenyYou
 );
 
 my @songsDone = qw(
@@ -16,6 +15,7 @@ my @songsDone = qw(
 02-ViolentByNature
 03-PainInside
 04-VisionsInMyHead
+08-DenyYou
 09-WithinADyingBreed
 10-MindsOfTheWorld
 11-OutOfBlood
