1f1210083932a82938339abc42d38eaa8b308997
Russ Johnson <nofuss@gmail.com>
2020-11-22 13:08:58 +0100

    setting up Obituary's latest album

---
+++ Obituary/Obituary/select.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-Brave
+02-SentenceDay
+03-ALessonInVengeance
+04-EndItNow
+05-KneelBeforeMe
+06-ItLives
+07-Betrayed
+08-TurnedToStone
+09-StraightToHell
+10-TenThousandWaysToDie
+11-NoHope
+);
+
+my @songsDone = qw(
+
+);
+
+for my $dir (@songsToDo) {
+    print `mkdir $dir`;
+    print `touch $dir/info.txt`;
+}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
+
