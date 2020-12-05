123670967983813d8ee13a32d9384d5c9cebc2a6
Russ Johnson <nofuss@gmail.com>
2020-12-05 12:21:09 +0100

    setting up career objective for som emore serious work

---
+++ TheMethadones/CareerObjective/select.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-PrematureMidLifeCrisis
+02-ImAboutToCrack 	
+03-SayGoodbyeToYourGeneration 	
+04-AreYouReallyForReal 	
+05-Ammunition 	
+06-FarAway 	
+07-StuckInMyHead 	
+08-Revitalized
+09-Antidote 	
+10-TVWorld 	
+11-YouDontKnowMeAnymore
+12-AccidentWaitingToHappen 	
+13-Examine
+);
+
+my @songsDone = qw(
+);
+
+for my $dir (@songsToDo) {
+    print `mkdir $dir`;
+    print `touch $dir/info.txt`;
+}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
+
