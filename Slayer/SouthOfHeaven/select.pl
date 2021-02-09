fe26676cb02ca0b5ad5fea676f70bf43b9e63671
Russ Johnson <nofuss@gmail.com>
2021-02-09 10:32:31 +0100

    getting started on mandatory suicide

--- Slayer/SouthOfHeaven/select.pl
+++ Slayer/SouthOfHeaven/select.pl
@@ -5,9 +5,7 @@ use warnings;
 my @songsToDo = qw(
 01-SouthOfHeaven
 02-SilentScream
-03-LiveUndead
 04-BehindTheCrookedCross
-05-MandatorySuicide
 06-GhostsOfWar
 07-ReadBetweenTheLies
 08-CleanseTheSoul
@@ -16,7 +14,8 @@ my @songsToDo = qw(
 );
 
 my @rhythmDone = qw(
-
+03-LiveUndead
+05-MandatorySuicide
 );
 
 my @leadDone = qw(
