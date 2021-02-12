1c5afa5fc5f849c7b84e1244b3f136460073fd46
Russ Johnson <nofuss@gmail.com>
2021-02-12 14:02:44 +0100

    setting up back in black

---
+++ ACDC/BackInBlack/select.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-HellsBells
+02-ShootToThrill
+03-WhatDoYouDoForMoneyHoney
+04-GivenTheDogABone
+05-LetMePutMyLoveIntoYou
+06-BackInBlack
+07-YouShookMeAllNightLong
+08-HaveADrinkOnMe
+09-ShakeALeg
+10-RockAndRollAintNoisePollution
+);
+
+my @songsStarted = qw(
+
+);
+
+my @songsWithLead = qw(
+
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+    #print `touch $song/info.txt`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
