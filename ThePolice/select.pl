ff5a425c9425dad90ab09757fe46e0055a83826c
Russ Johnson <nofuss@gmail.com>
2021-05-15 22:09:00 +0200

    sorted things out and added bass tab files

--- ThePolice/select.pl
+++ ThePolice/select.pl
@@ -3,31 +3,31 @@ use strict;
 use warnings;
 
 my @songsToDo = qw(
-Roxanne
-CantStandLosingYou
-SoLonely
-MessageInABottle
-WalkingOnTheMoon
-TheBedsTooBigWithoutYou
-DontStandSoClosetoMe
-DeDoDoDoDeDaDaDa
-EveryLittleThingSheDoesIsMagic
-InvisibleSun
-SpiritsInTheMaterialWorld
-SynchronicityII
-EveryBreathYouTake
-KingOfPain
-WrappedAroundYourFinger
-TeaInTheSahara
+01-Roxanne
+02-CantStandLosingYou
+03-SoLonely
+04-MessageInABottle
+05-WalkingOnTheMoon
+06-TheBedsTooBigWithoutYou
+07-DontStandSoClosetoMe
+09-EveryLittleThingSheDoesIsMagic
+10-InvisibleSun
+11-SpiritsInTheMaterialWorld
+12-SynchronicityII
+13-EveryBreathYouTake
+14-KingOfPain
+15-WrappedAroundYourFinger
+16-TeaInTheSahara
 );
 
 my @songsDone = qw(
-
+08-DeDoDoDoDeDaDaDa
 );
 
-#for my $song (@songsToDo) {
-    #print `mkdir $song`;
-    #print `cp tabs.et6 $song`;
-#}
+for my $song (@songsToDo) {
+    print `mkdir $song`;
+    print `cp guitar-tabs.et6 $song`;
+    print `cp bass-tabs.et4 $song`;
+}
 
 print $songsToDo[rand @songsToDo] . "\n";
