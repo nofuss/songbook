ffbd0984dfd9c014e7f966bd2d82c1bc18a87212
Russ Johnson <nofuss@gmail.com>
2021-10-19 20:58:44 +0200

    getting another album set up

---
+++ TeenageBottlerocket/SickSesh/select.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-SemiTruck
+02-YoureNeverGoingOutOfStyle
+03-GhostStory
+04-NeverSingAlong
+05-StrungOutOnStress
+06-Statistic
+07-HelloDana
+08-SpendTheNight
+09-TheSquirrel
+10-TheoreticalReality
+11-GorillaWarfare
+12-MovingOn
+);
+
+my @songsDone = qw(
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `cp tabs.et6 $song`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
