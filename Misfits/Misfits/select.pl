13b4f1c8142eea7538ab80d8e40c2169b63a7709
Russ Johnson <nofuss@gmail.com>
2021-12-27 17:46:50 +0100

    setting up another Misfits albumn

---
+++ Misfits/Misfits/select.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+101-She
+102-HollywoodBabylon
+103-Bullet
+104-HorrorBusiness
+105-TeenagersFromMars
+106-NightOfTheLivingDead
+107-WhereEaglesDare
+108-Vampira
+109-ITurnedIntoAMartian
+110-Skulls
+201-LondonDungeon
+202-GhoulsNightOut
+203-AstroZombies
+204-MommyCanIGoOutAndKillTonight
+205-DieDieMyDarling
+206-EarthAD
+207-Devilock
+208-DeathComesRipping
+209-GreenHell
+210-WolfsBlood
+);
+
+my @songsDone = qw(
+);
+
+#for my $song (@songsToDo) {
+    #print `mkdir $song`;
+    #print `touch $song/info.txt`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
