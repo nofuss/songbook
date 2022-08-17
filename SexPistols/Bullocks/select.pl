7283977190928fccbc86ff170c0957dbd15d70eb
Russ Johnson <nofuss@gmail.com>
2022-08-17 12:47:56 +0200

    cleaned up submission

--- SexPistols/Bullocks/select.pl
+++ SexPistols/Bullocks/select.pl
@@ -9,13 +9,13 @@ my @songsToDo = qw(
 06-Problems
 07-Seventeen
 08-AnarchyInTheUK
-09-Submission
 11-NewYork
 );
 
 my @songsFirstRound = qw(
 03-NoFeelings
 05-GodSaveTheQueen
+09-Submission
 10-PrettyVacant
 12-EMI
 );
