1a0fa740bcef9e12955bfc27f4d3e7e2d628b58a
Russ Johnson <nofuss@gmail.com>
2022-09-07 20:15:42 +0200

    cleaned up confessional

--- Deicide/OnceUponTheCross/select.pl
+++ Deicide/OnceUponTheCross/select.pl
@@ -5,7 +5,6 @@ use warnings;
 my @songsToDo = qw(
 05-TrickOrBetrayed
 08-ToBeDead
-09-ConfessionalRape
 );
 
 my @songsStarted = qw(
@@ -14,6 +13,7 @@ my @songsStarted = qw(
 04-KillTheChristian
 06-TheyAreTheChildrenOfTheUnderworld
 07-BehindTheLightThouShallRise
+09-ConfessionalRape
 );
 
 my @songsDone = qw(
@@ -25,7 +25,7 @@ my @songsDone = qw(
     #print `cp tabs.et6 $song`;
 #}
 
-print $songsStarted[rand @songsStarted] . "\n";
-#print $songsToDo[rand @songsToDo] . "\n";
+#print $songsStarted[rand @songsStarted] . "\n";
+print $songsToDo[rand @songsToDo] . "\n";
 
 
