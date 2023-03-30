256c9975da24e91ad0d0b92676e65ff4d076a34b
Russ Johnson <nofuss@gmail.com>
2023-03-30 18:43:19 +0200

    set up static x project

---
+++ StaticX/WisconsinDeathTrip/select.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @songsToDo = qw(
+01-PushIt
+02-ImWithStupid
+03-BledForDays
+04-LoveDump
+05-IAm
+06-Otsegolation
+07-Stem
+08-SweatOfTheBud
+09-Fix
+10-WisconsinDeathTrip
+11-TheTranceIsTheMotion
+12-December
+13-Down
+);
+
+my @songsDone = qw(
+
+);
+
+#for my $dir (@songsToDo) {
+    #print `mkdir $dir`;
+    #print `cp tabs.et6 $dir/tabs.et6`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
+
