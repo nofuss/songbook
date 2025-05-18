40be3a6a5b61749ebcdc039858d1e541409d16f3
Russ Johnson <nofuss@gmail.com>
2025-05-18 23:01:58 +0200

    worked out by the dawn tabs

--- Obituary/DyingOfEverything/select.pl
+++ Obituary/DyingOfEverything/select.pl
@@ -5,7 +5,6 @@ use warnings;
 srand(time());
 
 my @songsToDo = qw(
-07-ByTheDawn
 08-WeaponizeTheHate
 09-TornApart
 );
@@ -17,6 +16,7 @@ my @songsDone = qw(
 04-War
 05-DyingOfEverything
 06-MyWillToLive
+07-ByTheDawn
 10-BeWarned
 );
 
