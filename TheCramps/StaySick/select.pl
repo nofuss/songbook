f77118b7d1e99ac6ffe9c189d0c9a63fb2474921
Russ Johnson <nofuss@gmail.com>
2026-09-08 23:46:41 +0200

    setting up stay sick

---
+++ TheCramps/StaySick/select.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+srand(time());
+
+my @songsToDo = qw(
+01-BopPills
+02-GodDamnRockRoll
+03-BikiniGirlsWithMachineGuns
+04-AllWomenAreBad
+05-TheCreatureFromTheBlackLeatherLagoon
+06-ShortninBread
+07-DaisysUpYourButterfly
+08-EverythingGoes
+09-JourneyToTheCenterOfAGirl
+10-MamaOoPowPow
+11-SaddleUpABuzzBuzz
+12-MuleskinnerBlues
+);
+
+my @songsSkipped = qw(
+);
+
+my @songsDone = qw(
+);
+
+#for my $dir (@songsToDo) {
+    #print `mkdir $dir`;
+    #print `cp ../tabs.et6 $dir/tabs-guitar.et6`;
+    #print `cp ../tabs.et4 $dir/tabs-bass.et4`;
+    #print `touch $dir/info.txt`;
+    #print `touch $dir/lyrics.txt`;
+#}
+
+print $songsToDo[rand @songsToDo] . "\n";
+
+
